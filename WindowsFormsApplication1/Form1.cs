6532a783b4bf21a502d7af55fe04011a63cbc253
Gianfranco Balzano <balzano.gianfranco@gmail.com>
2016-05-09 17:38:11 +0200

    Aggiunta aggiornameto schermo in tempo reale

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -9,14 +9,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using System;
+
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
+
 using System.Threading;
 
 namespace WindowsFormsApplication1
 {
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -26,7 +27,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
             //AsynchronousSocketListener.StartListening();
             int parsedValue;
             if (!int.TryParse(textBox1.Text, out parsedValue))
@@ -41,7 +42,9 @@ namespace WindowsFormsApplication1
                 return;
             }
             object poo = (object)porta;
-            Thread workerThread = new Thread(AsynchronousSocketListener.StartListening);
+            Thread updateThread = new Thread(aggiorna_video);
+            Thread workerThread = new Thread(StartListening);
+            updateThread.Start();
             workerThread.Start(poo);
             Console.WriteLine("main thread: Starting StartListening...");
         }
@@ -52,11 +55,13 @@ namespace WindowsFormsApplication1
             textBox2.Text = ipServer.AddressList[0].ToString();
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e) {
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
         }
 
 
-        private void textBox1_TextChanged(object sender, EventArgs e) {
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -81,11 +86,206 @@ namespace WindowsFormsApplication1
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
+        }
+
+
+        public static string[] parametri;
+        public static bool new_parameters = false;
+
+        public void aggiorna_video ()
+        {
+            while (true)
+            {
+                if (new_parameters)
+                {
+                    try
+                    {
+                        textBox3.Text = parametri[0];
+                        textBox4.Text = parametri[1];
+                        textBox5.Text = parametri[2];
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Write(e.ToString());
+                    }
+                }
+            }
+        }
+
+        
+
+
+        // Thread signal.
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public static void StartListening(object port)
+        {
+            bool ris;
+            // Data buffer for incoming data.
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.
+            // The DNS name of the computer
+            // running the listener is "host.contoso.com".
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+            Console.WriteLine(ipAddress.ToString());
+            int popo = (int)port;
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, popo);
+
+
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            ris = SocketConnected(listener);
+
+            Console.WriteLine(ris.ToString());
+
+            // Bind the socket to the local endpoint and listen for incoming connections.
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections.
+                    Console.WriteLine("Waiting for a connection...");
+                    listener.BeginAccept(
+                        new AsyncCallback(AcceptCallback),
+                        listener);
+
+                    // Wait until a connection is made before continuing.
+                    allDone.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
+
+
+        public static bool SocketConnected(Socket s)
+        {
+            bool part1 = s.Poll(1000, SelectMode.SelectRead);
+            bool part2 = (s.Available == 0);
+            if (part1 && part2)
+                return false;
+            else
+                return true;
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Get the socket that handles the client request.
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // Signal the main thread to continue.
+            allDone.Set();
+
+            // Create the state object.
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReadCallback), state);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket
+            // from the asynchronous state object.
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            // Read data from the client socket. 
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+
+                // Check for end-of-file tag. If it is not there, read 
+                // more data.
+                content = state.sb.ToString();
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    parametri = content.Split('#');
+                    new_parameters = true;
+
+                    // All the data has been read from the 
+                    // client. Display it on the console.
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                        content.Length, content);
+                    // Echo the data back to the client.
+                    Send(handler, content);
+                }
+                else
+                {
+                    // Not all data received. Get more.
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.
+            handler.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                Socket handler = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static int MainPeppe(String[] args, object port)
+        {
+            StartListening(port);
+            return 0;
         }
     }
 
-    public class StateObject
+
+
+public class StateObject
     {
         // Client  socket.
         public Socket workSocket = null;
@@ -97,8 +297,11 @@ namespace WindowsFormsApplication1
         public StringBuilder sb = new StringBuilder();
     }
 
-    public class AsynchronousSocketListener
+/*    public class AsynchronousSocketListener
     {
+        public static string[] parametri;
+        public static bool new_parameters = false;
+
         // Thread signal.
         public static ManualResetEvent allDone = new ManualResetEvent(false);
 
@@ -212,6 +415,10 @@ namespace WindowsFormsApplication1
                 content = state.sb.ToString();
                 if (content.IndexOf("<EOF>") > -1)
                 {
+                    parametri = content.Split('#');
+                    new_parameters = true;
+                    Form1.aggiorna_video();
+
                     // All the data has been read from the 
                     // client. Display it on the console.
                     Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
@@ -265,7 +472,7 @@ namespace WindowsFormsApplication1
             return 0;
         }
 
-    }
+    }*/
 
 
 }
