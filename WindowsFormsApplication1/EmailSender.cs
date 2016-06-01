36675423aa9ab1272272f5e51a5b0e85a56e0692
Fabio <fabiopalumbo@msn.com>
2016-06-01 16:39:48 +0200

    Rivedere invio mail

--- WindowsFormsApplication1/EmailSender.cs
+++ WindowsFormsApplication1/EmailSender.cs
@@ -16,14 +16,14 @@ namespace WindowsFormsApplication1
             string from = "mrc.gruppo@libero.it";
             MailMessage message = new MailMessage(from, to);
             message.Subject = "Allerta superamento soglie nel tuo centro";
-            message.Body = @"E' stato rilevato un superamento delle soglie relative alle condiioni ambientali ottimali nel tuo centro. I dati rilevati sono \n"
-                             + "Temperatura: " + measuredTemp + "\n Pressione: " + measuredPres + "\n Umidità: " + measuredPres;
+            message.Body = "Ciao";//@"E' stato rilevato un superamento delle soglie relative alle condiioni ambientali ottimali nel tuo centro. I dati rilevati sono \n"
+            //                 + "Temperatura: " + measuredTemp + "\n Pressione: " + measuredPres + "\n Umidità: " + measuredPres;
             SmtpClient client = new SmtpClient("smtp.libero.it", 465);
-            client.EnableSsl = true;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = false;
             NetworkCredential basicCredential = new NetworkCredential("mrc.gruppo@libero.it", "nuncfermnisciun2016");
             client.Credentials = basicCredential;
+            client.EnableSsl = true;
 
             try
             {
